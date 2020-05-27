289cb494d8eaae961823022aa446ad40c92726b5
mattted <medward4@gmail.com>
2020-05-26 20:07:32 -0700

    Add observation model and relationships

--- api/app/models/county.rb
+++ api/app/models/county.rb
@@ -1,3 +1,5 @@
 class County < ApplicationRecord
   belongs_to :state
+  has_many :observations
+  has_many :organisms, through: :observations
 end
---
+++ api/db/migrate/20200527025843_create_observations.rb
@@ -0,0 +1,15 @@
+class CreateObservations < ActiveRecord::Migration[6.0]
+  def change
+    create_table :observations do |t|
+      t.string :uname
+      t.datetime :date
+      t.float :lat
+      t.float :lon
+      t.st_point :geom, srid: 4269
+      t.references :observation, foreign_key: true
+      t.references :county, foreign_key: true
+    end
+    
+    add_index :observations, :geom, using: :gist
+  end
+end
