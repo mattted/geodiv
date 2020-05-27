07ed2c0873ee11a42d34ea4b4e3c85866fac1f07
mattted <medward4@gmail.com>
2020-05-26 19:05:32 -0700

    Create sql for observation table

---
+++ api/db/inat/create_observations.sql
@@ -0,0 +1,16 @@
+CREATE TABLE observations (
+	link VARCHAR, 
+	uname VARCHAR, 
+	date TIMESTAMP, 
+	lat DECIMAL, 
+	lon DECIMAL, 
+	country VARCHAR, 
+	state VARCHAR, 
+	sciname VARCHAR, 
+	kingdom VARCHAR, 
+	phylum VARCHAR, 
+	class VARCHAR, 
+	orden VARCHAR, 
+	family VARCHAR, 
+	genus VARCHAR
+);
