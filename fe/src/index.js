6cb03bbb16f80b8b53ec52c5ca6323c5a29a2b53
mattted <medward4@gmail.com>
2020-05-31 22:03:21 -0700

    Add filter class to manage datalist inputs

--- fe/src/index.js
+++ fe/src/index.js
@@ -2,7 +2,7 @@ require('./mystyles.scss')
 import "@babel/polyfill";
 import Map from './map.js'
 import API from './api.js'
-import Factory from './factory.js'
+import Filter from './filter.js'
 import * as d3 from 'd3';
 
 // load map
@@ -10,9 +10,14 @@ const map = new Map(600, 900, '#map')
 map.mountData('counties')
 
 // load map datalist filters
-Factory.kindgomDatalist()
-Factory.cnameDatalist()
-
+Filter.setFilterListeners()
+Filter.kindgomDatalist()
+Filter.cnameDatalist()
 
 document.querySelector('#by_org').addEventListener('click', () => map.mountData('counties', 'org_per_county'))
 document.querySelector('#by_obs').addEventListener('click', () => map.mountData('counties', 'obs_per_county'))
+document.querySelector('.control').addEventListener('select', e => console.log(e.target.parentNode.children))
+
+function test(x) {
+  console.log(x) 
+}
