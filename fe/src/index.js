f496b1304d507c75c17dbdaf4016c169d803ec77
mattted <medward4@gmail.com>
2020-06-01 19:54:32 -0700

    Fix map redraw problem

--- fe/src/index.js
+++ fe/src/index.js
@@ -8,12 +8,19 @@ import Filter from './filter.js'
 // load map
 const map = new Map(600, 900, '#map')
 map.mountData('counties')
+  .then(mapData => map.drawMap())
 
 // load map datalist filters
 Filter.populateDatalist('kingdom')
 
-document.querySelector('#by_org').addEventListener('click', () => map.mountData('none', 'org_per_county'))
-document.querySelector('#by_obs').addEventListener('click', () => map.mountData('none', 'obs_per_county'))
+document.querySelector('#by_org').addEventListener('click', () => {
+  map.mountData('none', 'org_per_county')
+  .then(mapData => map.drawMap())
+})
+document.querySelector('#by_obs').addEventListener('click', () => {
+  map.mountData('none', 'obs_per_county')
+    .then(mapData => map.drawMap())
+})
 
 document.querySelectorAll('button.filter').forEach(button => button.addEventListener('click', e => {
   Array.from(e.target.parentElement.children).forEach(button => button.classList.add('is-outlined'))
@@ -26,4 +33,5 @@ document.querySelector('#mapfilter').addEventListener('select', e => {
   let search = e.target.value
   let column = document.querySelector('div.buttons').getAttribute('selected')
   map.mountData('none', `obs_by_query?search=${search};column=${column}`)
+    .then(mapData => map.drawMap())
 });
--- fe/src/map.js
+++ fe/src/map.js
@@ -12,6 +12,9 @@ export default class Map {
       .attr("width", this.width)
       .attr("height", this.height)
     this.boundary = this.svg.append('g').attr('class', 'boundary')
+    this.legendGroup = this.svg.append("g")
+      .attr("class", "legendGroup")
+      .attr("transform", "translate(20,20)")
     this.graticule = d3.geoGraticule()
       .extent([[-98 - 80, 38 - 45], [-98 + 35, 38 + 45]])
       .step([5, 5]);
@@ -23,7 +26,9 @@ export default class Map {
     if(geo !== 'none') this.geoData = await API.fetchGeo(geo)
     if(metric !== 'none') this.metricData = await API.fetchMetric(metric)
     if(this.metricData !== {}) this.bindMetric()
+    this.setMetricName(metric)
     this.setBounds(this.geoData)
+    return this.geoData
   }
 
   setBounds(focusGeo) {
@@ -38,7 +43,6 @@ export default class Map {
       (this.height - s * (b[1][1] + b[0][1])) /2]
     // set overall projection scale
     this.projection.scale(s).translate(t)
-    this.drawMap()
   }
 
   bindMetric() {
@@ -51,15 +55,43 @@ export default class Map {
       .range(["#d0d0d0", "#3b4252"])
   }
 
+  setMetricName(metric){
+    if(metric === 'obs_per_county') this.queryCol = 'Total observations'
+    else if(metric === 'org_per_county') this.queryCol = 'Total unique organisms'
+    else if(metric === 'none') return
+    else {
+      let url = metric.split(/[=?;]/)
+      let search = url[2]
+      let col = url[4]
+      switch(col) {
+        case 'klass':
+          col = 'Class'
+          break;
+        case 'cname':
+          col = 'Common Name'
+          break;
+        default:
+          col = col.charAt(0).toUpperCase() + col.slice(1)
+      }
+      this.queryCol = col
+      this.querySearch = search
+    }
+  }
+
+  drawLegend() {
+    this.legendTitle = this.legendGroup.append('text')
+      .attr('class', 'legendTitle')
+      .text(`Observations of ${this.queryCol} ${this.querySearch}`)
+  }
+
   drawMap() {
-    // draw graticules
     this.svg.selectAll('path')
       .data(this.graticule.lines())
       .enter().append('path')
       .attr('class', 'graticule')
       .attr('d', this.path)
 
-    this.boundary = this.svg.selectAll('.boundary')
+    this.boundary = this.svg.selectAll('.counties')
       .data(this.geoData.features)
 
     this.boundary.enter().append('path')
@@ -70,13 +102,14 @@ export default class Map {
         return this.colorScale(d.properties.metric)
       })
 
-    this.boundary.transition().duration(500)
+    this.boundary.exit().remove()
+    
+    this.boundary.transition().duration(1000)
       .attr('d', this.path)
       .attr('fill', d => {
         if(this.colorScale(d.properties.metric) == undefined) return "#f0f0f0"
         return this.colorScale(d.properties.metric)
       })
 
-    this.boundary.exit().remove();
   }
 }
